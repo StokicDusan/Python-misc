563d81ade45a5f9f7292b72fcea2b6f14a7086ef
ConusDaktis <stokicdusan@gmail.com>
2021-04-12 22:01:38 +0200

    reformating cont.py

--- AmicableNumbers/cont.py
+++ AmicableNumbers/cont.py
@@ -3,12 +3,13 @@ from math import fabs
 from decimal import Decimal
 from time import clock
 
+
 def Continue(n):
     while(1):
-        x=str(input('continue?(Y\\N) :'))
-        if x.upper()=="N":
+        x = str(input('continue?(Y\\N) :'))
+        if x.upper() == "N":
             return 0
-        elif x.upper()=="Y":
+        elif x.upper() == "Y":
             print()
             return 1
 
@@ -16,23 +17,25 @@ def Continue(n):
 class Stopwatch:
     def __init__(self):
         self.reset()
+
     def start(self):
         if not self.__running:
-            self.__start_time=clock()
-            self.__running=True
+            self.__start_time = clock()
+            self.__running = True
         else:
             print('Stopwatch already running')
+
     def stop(self):
         if self.__running:
-            self.__elapsed+=clock()-self.__start_time
-            self.__running=False
+            self.__elapsed += clock()-self.__start_time
+            self.__running = False
         else:
             print('Stopwatch not running')
-            
+
     def reset(self):
-        self.__start_time = self.__elapsed= 0
-        self.__running=False
-        
+        self.__start_time = self.__elapsed = 0
+        self.__running = False
+
     def elapsed(self):
         if not self.__running:
             return self.__elapsed
