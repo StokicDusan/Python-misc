91c1f27afff40ceeefcfb24106be2d856c340473
ConusDaktis <stokicdusan@gmail.com>
2021-04-13 16:38:39 +0200

    aded a prime check script

---
+++ PrimeCheck/primeCheck.py
@@ -0,0 +1,67 @@
+from math import sqrt
+import sys
+
+
+def factors(A, x):
+    print('%i = ' % x, end="")
+    k, i = 0, 0
+    while i < len(A):
+        n = A.count(A[i])
+        if n == 1:
+            print(f'{A[i]:,}', end="")
+            k += 1
+            i += 1
+        else:
+            print('{}^{}'.format(f'{A[i]}', n), end="")
+            s = A[i]
+            while s in A:
+                A.remove(s)
+        if k < len(A):
+            print(' * ', end="")
+    print()
+
+
+def all_prime_factors(p):
+    factors = []
+    loop = 2
+    x = p
+    while loop <= x:
+        if x % loop == 0:
+            x //= loop
+            factors.append(loop)
+        else:
+            loop += 1
+#        print('Searching(%.5f%%)' % (100*loop/x), end="\r")
+    factors.sort()
+    print('                           ', end="\r")
+    return factors
+
+
+def is_prime(pp):
+    global com
+    if pp == 1:
+        return False
+    trial = 2
+    root = sqrt(pp)
+    while trial <= root:
+#        print('Searching(%.5f%%)' % (100*trial/root), end="\r")
+        if pp % trial == 0:
+            print('This number is not prime')
+            com = all_prime_factors(pp)
+            return False
+        trial += 1
+    return True
+
+
+def main(argv):
+    global com
+    x = int(argv)
+    if is_prime(x):
+        print('This number is Prime!!!')
+    else:
+        factors(com, x)
+
+
+com = []
+if __name__ == "__main__":
+    main(sys.argv[1])
