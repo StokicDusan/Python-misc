e0d3c013c1489c0bf57b4d393ad8a90644eb97f1
ConusDaktis <stokicdusan@gmail.com>
2021-04-21 22:48:24 +0200

    minor changes

--- OVSFCode/VolsovoStablo.py
+++ OVSFCode/VolsovoStablo.py
@@ -1,3 +1,6 @@
+from math import log2
+
+
 def rev(A):
     for i in range(0, len(A)):
         A[i] = -A[i]
@@ -5,10 +8,10 @@ def rev(A):
 
 
 def fajl(K):
-    f = open("volse.txt", "w+")
+    f = open("vols.txt", "w+")
     for i in range(0, len(K)):
         for j in range(0, len(K[i])):
-            f.write('Code'+str(len(K[i]))+','+str(j)+str(K[i][j])+'\n')
+            f.write('C_'+str(len(K[i]))+','+str(j)+str(K[i][j])+'\n')
         f.write('\n')
     print('Done!')
     f.close()
@@ -18,7 +21,7 @@ def ispis(K):
     print()
     for i in range(0, len(K)):
         for j in range(0, len(K[i])):
-            print('Code'+str(len(K[i]))+','+str(j), end='=')
+            print('C_'+str(len(K[i]))+','+str(j), end='=')
             print(K[i][j])
         print()
 
@@ -38,9 +41,15 @@ def stablo(A):
 
 def main():
     global kod
-    n = int(input('Duzina Volsovog stabla:'))
-    stablo(n)
-    nn = int(input('1.Ispis\n2.Fajl\n'))
+    while 1:
+        nn = int(input('1.Print OVSF codes\n2.Save codes in file\n'))
+        if(nn == 1 or nn == 2):
+            break
+    while 1:
+        n = int(input('SF='))
+        if((n & (n-1) == 0) and n != 0):
+            break
+    stablo(int(log2(n)))
     if (nn == 1):
         ispis(kod)
     elif (nn == 2):
