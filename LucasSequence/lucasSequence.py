1a7a80e66d6763e62a0772b99e04ffd8b59191cf
StokicDusan <stokicdusan@gmail.com>
2021-05-04 12:30:26 +0200

    adding functions with optional timer and printing just last number in sequence

--- LucasSequence/lucasSequence.py
+++ LucasSequence/lucasSequence.py
@@ -2,6 +2,9 @@
 # such that L(n) = L(n-1) + L(n-2)
 
 from time import perf_counter
+from math import sqrt
+import sys
+
 
 class Stopwatch:
     def __init__(self):
@@ -33,58 +36,66 @@ class Stopwatch:
             return None
 
 
-def Continue(n):
-    while(1):
-        x=str(input('continue?(Y\\N) :'))
-        if x.upper()=="N":
-            return 0
-        elif x.upper()=="Y":
-            print()
-            return 1
-
-
-def main():
-    timer = Stopwatch()
-    while 1:
-        k = int(eval(input('Display:\n1.Last number\n2.Entire sequence\n')))
-        if k == 1 or k == 2:
-            break
-    L0, L1 = eval(input('Enter the first two values for the sequence:'))
-    n = eval(input('Enter the Length of the sequence:'))
-    timer.start()
-    if k == 1:
-        n = n+1
-        for i in range(0, n-3):
-            L0,L1 = L1,L0+L1
-    if k == 2:
-        print(L0, end=" ")
-        print(L1, end=" ")
-        print(L0+L1, end=" ")
-        for i in range(0, n-3):
-            L0,L1 = L1,L0+L1
-            print(L0+L1, end=" ")
-    timer.stop()
-    print()
-    if k == 1:
-        print(L1)
-#        print("The number has ",f'{len(str(L[n % 2])):,}'," digits")
+def print_time(time: float) -> None:
     print('\nElapsed: ', end="")
-    if timer.elapsed() > 1:
-        elapsed = round(timer.elapsed(), 3)
+    if time > 1:
+        elapsed = round(time, 3)
         print('%.3f s' % elapsed)
-    elif timer.elapsed() > 0.001:
-        elapsed = timer.elapsed()*1000
+    elif time > 0.001:
+        elapsed = time*1000
         elapsed = round(elapsed, 2)
         print('%.2f ms' % elapsed)
     else:
-        elapsed = timer.elapsed()*1000000
+        elapsed = time()*1000000
         elapsed = round(elapsed, 2)
         print('%.2f µs' % elapsed)
 
 
-c = 1
-while c:
-    if __name__ == "__main__":
-        main()
-        c = Continue(c)
-        print()
+def lucas_sequence(n0: int, n1: int, n2: int) -> None:
+    L0, L1 = n0, n1
+    if n2 >= 1:
+        print(L0, end=" ")
+    if n2 >= 2:
+        print(L1, end=" ")
+    for i in range(0, n2-2):
+        print(L0+L1, end=" ")
+        L0, L1 = L1, L0+L1
+
+
+def lucas_sequence_timer(n0: int, n1: int, n2: int) -> None:
+    timer = Stopwatch()
+    timer.start()
+    L0, L1 = n0, n1
+    if n2 >= 1:
+        print(L0, end=" ")
+    if n2 >= 2:
+        print(L1, end=" ")
+    for i in range(0, n2-2):
+        print(L0+L1, end=" ")
+        L0, L1 = L1, L0+L1
+    timer.stop()
+    print_time(timer.elapsed)
+
+
+def lucas_sequence_last(n0: int, n1: int, n2: int) -> None:
+    L0, L1 = n0, n1
+    for i in range(0, n2-2):
+        L0, L1 = L1, L0+L1
+    print(L1)
+
+
+def lucas_sequence_last_timer(n0: int, n1: int, n2: int) -> None:
+    timer = Stopwatch()
+    timer.start()
+    L0, L1 = n0, n1
+    for i in range(0, n2-2):
+        L0, L1 = L1, L0+L1
+    print(L1)
+    timer.stop()
+    print_time(timer.elapsed)
+
+
+
+if __name__ == "__main__":
+    if(sys.argv[3:]):
+        lucas_sequence(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
