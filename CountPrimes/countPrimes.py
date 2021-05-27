bf3728474d48d861124037b39687b117696f8a3a
StokicDusan <stokicdusan@gmail.com>
2021-05-27 12:56:01 +0200

    change variable names

--- CountPrimes/countPrimes.py
+++ CountPrimes/countPrimes.py
@@ -35,23 +35,23 @@ class Stopwatch:
 
 
 def TCS(yy):
-    global x, p, twin, cousin, sexy, prime, mmm
-    x += 1
+    global pythagoreanPrimeCount, twinPrimeCount
+    global cousinPrimeCount, sexyPrimeCount, prime, mmm
     if (yy-1)/4 == ((yy-1)/4)//1:
-        p += 1
+        pythagoreanPrimeCount += 1
     for r in prime:
         if yy-r == 2:
-            twin += 1
+            twinPrimeCount += 1
         elif yy-r == 4:
-            cousin += 1
+            cousinPrimeCount += 1
         elif yy-r == 6:
-            sexy += 1
+            sexyPrimeCount += 1
     prime[mmm % 3] = yy
     mmm += 1
 
 
 def mersenne(yy):
-    global m, Mersenne_Primes
+    global mersennePrimeCount, Mersenne_Primes
     if log2(yy+1) == log2(yy+1)//1:
         trial_factor = 2
         is_Mersenne = True
@@ -63,12 +63,11 @@ def mersenne(yy):
         if is_Mersenne:
             if yy not in Mersenne_Primes:
                 Mersenne_Primes += [yy]
-                m += 1
+                mersennePrimeCount += 1
 
 
 def count(pr: int, max_value: int) -> int:
-    global x, m, twin, cousin, sexy, mmm, p
-    global prime, Mersenne_Primes, timer
+    global allPrimeCount, timer
     timer.start()
     nonprimes = max_value * [False]
     nonprimes[0] = nonprimes[1] = True
@@ -80,6 +79,7 @@ def count(pr: int, max_value: int) -> int:
     if pr == 2 or pr == 3:
         for i in range(2, max_value):
             if not nonprimes[i]:
+                allPrimeCount += 1
                 TCS(i)
                 mersenne(i)
                 for j in range(2*i, max_value, i):
@@ -110,17 +110,19 @@ def test_main():
 
 
 def display(n):
-    global x, m, Mersenne_Primes, twin, count, sexy, p, timer
-    print('There are', f'{x:,}', 'Primes!!!!')
-    print('%.3f' % (x*100/n), '% are Primes', sep="")
-    print('There are', f'{m:,}', 'Mersenne Primes { Mp=2**p-1 }', end=": ")
+    global allPrimeCount, mersennePrimeCount, Mersenne_Primes
+    global twinPrimeCount, cousinPrimeCount
+    global sexyPrimeCount, pythagoreanPrimeCount, timer
+    print('There are', f'{allPrimeCount:,}', 'Primes!!!!')
+    print('%.3f' % (allPrimeCount*100/n), '% are Primes', sep="")
+    print('There are', f'{mersennePrimeCount:,}', 'Mersenne Primes { Mp=2**p-1 }', end=": ")
     for i in Mersenne_Primes:
         print(i, end=" ")
     print()
-    print('There are', f'{twin:,}', 'Twin Primes')
-    print('There are', f'{cousin:,}', 'Cousin Primes')
-    print('There are', f'{sexy:,}', 'Sexy Primes')
-    print('There are', f'{p:,}', 'Pythagorean Primes { Pp=4n+1 }', end="")
+    print('There are', f'{twinPrimeCount:,}', 'Twin Primes')
+    print('There are', f'{cousinPrimeCount:,}', 'Cousin Primes')
+    print('There are', f'{sexyPrimeCount:,}', 'Sexy Primes')
+    print('There are', f'{pythagoreanPrimeCount:,}', 'Pythagorean Primes { Pp=4n+1 }', end="")
     print()
     print('Elapsed:', round(timer.elapsed(), 6), 'seconds')
 
@@ -131,10 +133,18 @@ def main(argv1, argv2):
         display(n)
 
 
-x, m, twin, cousin, sexy, mmm, p = 0, 0, 0, 0, 0, 0, 0
+allPrimeCount=0
+mersennePrimeCount=0
+twinPrimeCount=0
+cousinPrimeCount=0
+sexyPrimeCount=0
+mmm=0
+pythagoreanPrimeCount = 0
 prime = [3, 5, 7]
 Mersenne_Primes = []
 timer = Stopwatch()
+
+
 if __name__ == "__main__":
     if(sys.argv[1:] and sys.argv[2:]):
         main(int(sys.argv[1]), int(sys.argv[2]))
